e2623e58f08221196447ae5e0d41748422e1af51
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-06-14 19:27:27 +0200

    [ADD] Organizando y creando las rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainLayout from "./Layouts/MainLayout";
+import Card from "./pages/card";
+import Mainpage from "./pages/mainpage";
+
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Mainpage/>} />
+          <Route path="card/:id" element={<Card/>} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
