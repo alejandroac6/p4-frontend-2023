474daf9953df293bdd380acbc8c5c2acda295a68
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-06-16 09:19:02 +0200

    [ADD añadir ruta dinamica al pulsar la card

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -1,10 +1,15 @@
+import {Link} from 'react-router-dom'
+
 const CardList = ({ card }) => {
-  const { imageUrl, name, manaCost, originalType, text } = card;
+  const { imageUrl, name, manaCost, originalType, text, id } = card;
 
   return (
     <>
       <div>
-        <img src={imageUrl} alt="Imagen" />
+        <Link to={`/card/${id}`}>
+            <img src={imageUrl} alt="Imagen" />        
+        </Link>
+        
 
         <div>
           <p>{name}</p>
--- src/hook/useMagic.tsx
+++ src/hook/useMagic.tsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const useMagic = () => {
+const useMagic = (id) => {
   const [cards, setCards] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://api.magicthegathering.io/v1/cards');
+        const response = await axios.get(`https://api.magicthegathering.io/v1/cards/${id}`);
         const filteredCards = response.data.cards.filter(card => card.imageUrl);
         setCards(filteredCards);
         setLoading(false);
