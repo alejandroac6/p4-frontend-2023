f23d1be353d143f56c4f04114be11b06b6569e66
Alejandro Alarcon <alejandro.alarcon.cano@gmail.com>
2023-06-19 10:24:14 +0200

    [ADD] Logos on main page

--- src/pages/mainpage.tsx
+++ src/pages/mainpage.tsx
@@ -1,10 +1,59 @@
 import useMagic from "../hook/useMagic";
 import CardList from "../components/CardList";
+import { useState } from "react";
+
 const Mainpage = () => {
   const { cards, loading } = useMagic({});
+  const[color,setColor]=useState('');
+  const[type,setType]=useState('');
+  const [isSelected, setIsSelected] = useState(false);
+
+  const imageStyle={
+    border: isSelected ? '0.2rem solid white':'0.2rem solid black'
+  }
+
+  const handleClick=()=>{
+    setIsSelected(!isSelected);
+  }
+
 
   return (
     <>
+      <div className="logo-container">
+        <div onClick={handleClick}>
+          <img
+            className="mana"
+            src="https://img2.freepng.es/20180615/tgi/kisspng-magic-the-gathering-magic-points-playing-card-car-werewolf-symbol-5b242521549546.0886581015290954573465.jpg"
+            alt="Fuego"
+            style={imageStyle} 
+          />
+
+        </div>
+
+        <img
+          className="mana"
+          src="https://img2.freepng.es/20180524/rgi/kisspng-magic-the-gathering-pro-tour-magic-points-wizards-gather-5b071a0f25d843.717925511527192079155.jpg"
+          alt="Bosque"
+          onClick={handleClick}
+          style={imageStyle}
+        />
+        <img
+          className="mana"
+          src="https://img2.freepng.es/20180422/cbe/kisspng-magic-the-gathering-mana-magic-points-symbol-play-5adcb4cd8ccae6.3447432815244136455767.jpg"
+          alt="Agua"
+        />
+        <img
+          className="mana"
+          src="https://img2.freepng.es/20180417/how/kisspng-magic-the-gathering-pro-tour-magic-points-mana-pl-creature-5ad637b61ccf96.442804691523988406118.jpg"
+          alt="Pantano"
+        />
+        <img
+          className="mana"
+          src="https://e7.pngegg.com/pngimages/891/659/png-clipart-magic-the-gathering-magic-points-mana-playing-card-heroes-of-might-and-magic-ii-others-game-logo.png"
+          alt="Llanura"
+        />
+      </div>
+
       <div className="cards-distribution">
         {cards.length ? (
           <>
